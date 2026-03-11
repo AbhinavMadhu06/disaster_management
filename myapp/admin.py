de95f235121958c3e050b740ba0471fa5f5f26d3
AbhinavMadhu06 <abhinavmadhu0605@gmail.com>
2026-03-11 17:14:45 +0530

    Fix admin.py ModelNotFoundError and register all models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,20 +1,24 @@
 from django.contrib import admin
-
 from .models import *
-# Register your models here.
 
-admin.site.register(News_reporter)
+# Register your models here.
+admin.site.register(Login)
+admin.site.register(Public)
 admin.site.register(Coordinator)
 admin.site.register(Camp)
-admin.site.register(Guideline)
-admin.site.register(Complaint)
-admin.site.register(EmergencyRescue)
-admin.site.register(Stock)
-admin.site.register(News)
-admin.site.register(Notification)
-admin.site.register(Public)
 admin.site.register(Volunteer)
+admin.site.register(Notification)
+admin.site.register(Complaint)
+admin.site.register(Guideline)
+admin.site.register(News_reporter)
+admin.site.register(News)
+admin.site.register(Stock)
 admin.site.register(Collection)
+admin.site.register(EmergencyRescue)
 admin.site.register(EmergencyAlert)
 admin.site.register(Needs)
-admin.site.register(MedicalSupport)
+admin.site.register(Medicine)
+admin.site.register(MedicalRequest)
+admin.site.register(Services)
+admin.site.register(DonateGoods)
+admin.site.register(Chatbot)
