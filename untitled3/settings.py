7efcd62eb1e5d44b3e8a4541b72cc25aab426945
AbhinavMadhu06 <abhinavmadhu0605@gmail.com>
2026-03-11 17:34:51 +0530

    Remove WhiteNoise staticfiles_storage to resolve collectstatic post-processing crashes

--- untitled3/settings.py
+++ untitled3/settings.py
@@ -135,7 +135,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Cloudinary configuration
 CLOUDINARY_STORAGE = {
