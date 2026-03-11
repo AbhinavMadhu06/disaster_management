8197dcf7094f9d43b1eb9ac42e769436a2679ad3
AbhinavMadhu06 <abhinavmadhu0605@gmail.com>
2026-03-11 16:53:45 +0530

    Admin aded with some models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from . django .models import *
 # Register your models here.
+
+admin.site.register(News_reporter)
+admin.site.register(Coordinator)
+admin.site.register(Camp)
+admin.site.register(Guideline)
+admin.site.register(Complaint)
+admin.site.register(EmergencyRescue)
+admin.site.register(Stock)
+admin.site.register(News)
+admin.site.register(Notification)
+admin.site.register(Public)
+admin.site.register(Volunteer)
+admin.site.register(Collection)
+admin.site.register(EmergencyAlert)
+admin.site.register(Needs)
+admin.site.register(MedicalSupport)
