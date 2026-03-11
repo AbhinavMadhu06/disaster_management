83d6d21f8ec837d95ff4c34e2eba3be7a0c5317e
AbhinavMadhu06 <abhinavmadhu0605@gmail.com>
2026-03-11 17:16:19 +0530

    Admin models edited

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Login)
 admin.site.register(Public)
 admin.site.register(Coordinator)
 admin.site.register(Camp)
