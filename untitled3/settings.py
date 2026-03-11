6e190e26d284b13e14b354448d9c00e2169e140b
AbhinavMadhu06 <abhinavmadhu0605@gmail.com>
2026-03-11 17:31:21 +0530

    Downgrade WhiteNoise to CompressedStaticFilesStorage to fix DRF collectstatic crash

--- untitled3/settings.py
+++ untitled3/settings.py
@@ -135,8 +135,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-WHITENOISE_MANIFEST_STRICT = False
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Cloudinary configuration
 CLOUDINARY_STORAGE = {
